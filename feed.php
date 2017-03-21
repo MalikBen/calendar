99e71190396e95e29e8c0a779c67f8f5c26f8fcf
Malik Bensabre <malik.bensabre@yoomap.fr>
2017-03-21 15:37:12 +0100

    Feed query

--- feed.php
+++ feed.php
@@ -1,102 +1,53 @@
 <?php
-//var_dump($_POST);
-echo '[
-  {
-    "title": "Selena Mccullough",
-    "start": "2017-03-29T08:19:29",
-    "color": "#A7D2C8",
-    "allDay": false,
-    "description": "Eu id esse duis sint mollit ad consectetur esse fugiat ipsum voluptate deserunt proident. Irure nisi laborum officia incididunt eu voluptate."
-  },
-  {
-    "title": "Harrington Sampson",
-    "start": "2017-03-06T15:34:22",
-    "color": "#874C62",
-    "allDay": false,
-    "description": "Officia aute voluptate fugiat esse in officia. Laboris id minim id proident aute."
-  },
-  {
-    "title": "Loretta Stevens",
-    "start": "2017-03-05T02:16:27",
-    "color": "#C98474",
-    "allDay": false,
-    "description": "Aute nulla nulla ad dolore. Mollit ut id occaecat fugiat eiusmod velit nulla reprehenderit nulla non duis."
-  },
-  {
-    "title": "Sabrina Martin",
-    "start": "2017-03-23T00:51:07",
-    "color": "#F2D388",
-    "allDay": true,
-    "description": "Minim exercitation officia eu fugiat duis voluptate excepteur ullamco consequat duis veniam non. Eu dolore esse in esse."
-  },
-  {
-    "title": "Smith Benson",
-    "start": "2017-03-25T10:50:48",
-    "color": "#874C62",
-    "allDay": false,
-    "description": "Cillum do excepteur ea mollit elit cupidatat do ullamco deserunt dolore excepteur magna incididunt. Nulla do id sint ut enim ex qui."
-  },
-  {
-    "title": "Myrtle Case",
-    "start": "2017-03-21T00:38:59",
-    "color": "#A7D2C8",
-    "allDay": false,
-    "description": "Aliqua est occaecat magna consectetur. Sunt sunt minim laborum eu enim consequat."
-  },
-  {
-    "title": "Carmella Mcgee",
-    "start": "2017-03-13T19:51:07",
-    "color": "#C98474",
-    "allDay": false,
-    "description": "Irure ullamco do tempor aliqua labore aliqua aliquip non. Laborum voluptate dolor cillum qui id amet magna."
-  },
-  {
-    "title": "Estela Byrd",
-    "start": "2017-03-20T15:52:07",
-    "color": "#874C62",
-    "allDay": true,
-    "description": "Enim nulla duis mollit ad nisi tempor laborum sint labore laborum. Quis ea nostrud adipisicing commodo aute laboris aute non cillum culpa aliqua."
-  },
-  {
-    "title": "Thompson Rojas",
-    "start": "2017-03-19T13:49:15",
-    "color": "#874C62",
-    "allDay": false,
-    "description": "Fugiat irure eu reprehenderit ut ullamco elit irure voluptate laboris. Nulla mollit exercitation fugiat ea velit proident aliqua minim commodo magna velit do qui."
-  },
-  {
-    "title": "Jane Guerrero",
-    "start": "2017-03-04T19:12:53",
-    "color": "#F2D388",
-    "allDay": false,
-    "description": "Pariatur ea ut mollit velit est aliquip duis eiusmod elit elit. Nisi tempor adipisicing enim occaecat aliquip irure."
-  },
-  {
-    "title": "Berg Mcdonald",
-    "start": "2017-03-11T08:04:09",
-    "color": "#F2D388",
-    "allDay": false,
-    "description": "Proident consectetur voluptate esse reprehenderit ad excepteur et irure laboris commodo. Voluptate ea veniam sunt deserunt culpa ad aute consectetur adipisicing nulla ex do irure velit."
-  },
-  {
-    "title": "Poole Knowles",
-    "start": "2017-03-18T04:29:32",
-    "color": "#A7D2C8",
-    "allDay": false,
-    "description": "Quis do est duis labore eiusmod culpa. Dolor nisi adipisicing amet ex sint ipsum reprehenderit ad esse est reprehenderit."
-  },
-  {
-    "title": "Holt Slater",
-    "start": "2017-03-15T03:59:20",
-    "color": "#F2D388",
-    "allDay": true,
-    "description": "Pariatur ex do duis eu. Elit laboris duis aute sit duis exercitation adipisicing minim fugiat consequat incididunt."
-  },
-  {
-    "title": "Mcbride Evans",
-    "start": "2017-03-13T13:02:46",
-    "color": "#F2D388",
-    "allDay": false,
-    "description": "Eiusmod ea nostrud fugiat esse eu ullamco pariatur excepteur sit adipisicing proident excepteur excepteur cillum. Exercitation cupidatat veniam nulla laboris ut officia nostrud."
-  }
-]';
+$mysqli = new mysqli("localhost", "malik", "ma123lik456", "erp");
+if ($mysqli->connect_errno) {
+    echo "Echec lors de la connexion à MySQL : (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+/* <----- On récupère le congés */
+$sql = '
+	SELECT 
+		d.iddata,
+        d.title,
+		dv1.value as data_start,
+        dv2.value as date_end,
+        dv3.value_str as people,
+        dv4.value as description
+	FROM 
+		datas d
+	INNER JOIN datas_values dv1 ON (dv1.iddata = d.iddata AND dv1.iddata_field = 50)
+    INNER JOIN datas_values dv2 ON (dv2.iddata = d.iddata AND dv2.iddata_field = 51)
+    INNER JOIN datas_values dv3 ON (dv3.iddata = d.iddata AND dv3.iddata_field = 52)
+    INNER JOIN datas_values dv4 ON (dv4.iddata = d.iddata AND dv4.iddata_field = 53)
+	WHERE 
+		d.iddata_type = 7
+		AND
+		d.iddata_status = 25
+		AND
+		d.iduser_delete is null
+        AND
+        dv2.value > NOW()';
+
+$arrayCalendar = array();
+if($result = $mysqli->query($sql)){
+    while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+        $arrayCalendar[] = array(
+            'title' => $row['people'],
+            'start' => $row['data_start'],
+            'end' => $row['date_end'],
+            'color' => '#A7D2C8',
+            'allDay' => true,
+            'description' => utf8_encode($row['description'])
+        );
+    }
+}
+
+$result->free();
+/* Fin congé -----> */
+
+
+/* Fermeture de la connexion */
+$mysqli->close();
+
+echo json_encode($arrayCalendar, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE);
+?>
